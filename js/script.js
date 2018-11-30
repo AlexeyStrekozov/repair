1613244e19b0d34c54fac723ee875e60ce0506ca
unknown <strekozovalexey@gmail.com>
2018-11-30 15:55:23 +0600

    Добавил owl

--- js/script.js
+++ js/script.js
@@ -7,34 +7,55 @@ $(document).ready(function () {
     })
   })
 
-  $(document).ready(function () {
-      $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        nextArrow: '<div class="arrow arrow__right"></div>',
-        prevArrow: '<div class="arrow arrow__left"></div>',
-        responsive: [
-          {
-            breakpoint: 1199,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 994,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 769,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-      ]
-      });
-    });
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '<div class="arrow arrow__right"></div>',
+    prevArrow: '<div class="arrow arrow__left"></div>',
+    responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 994,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: ["<img src='../img/arrow-left.png'>",
+    "<img src='../img/arrow-right.png'>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    769: {
+      items: 1
+    },
+    800: {
+      items: 2
+    },
+    1050: {
+      items: 3
+    }
+  }
+}) 
